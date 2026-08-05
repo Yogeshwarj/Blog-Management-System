62f3967c742d5e9d9445bba6e8939bceaaac3bb4
Yogeshwaran <jyogeshwaran85@gmail.com>
2026-08-05 10:35:07 +0530

    Day 5 - Added Express GET and POST API routes

--- index.js
+++ index.js
@@ -4,9 +4,55 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the public folder
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve frontend files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Temporary blog data
+let blogs = [
+    {
+        id: 1,
+        title: "Welcome to My Blog",
+        author: "Yogeshwaran",
+        content: "This is my first blog post."
+    }
+];
+
+// GET Route - Get all blogs
+app.get("/api/blogs", (req, res) => {
+    res.json(blogs);
+});
+
+// POST Route - Create a new blog
+app.post("/api/blogs", (req, res) => {
+
+    const { title, author, content } = req.body;
+
+    if (!title || !author || !content) {
+        return res.status(400).json({
+            message: "All fields are required"
+        });
+    }
+
+    const newBlog = {
+        id: blogs.length + 1,
+        title,
+        author,
+        content
+    };
+
+    blogs.push(newBlog);
+
+    res.status(201).json({
+        message: "Blog created successfully",
+        blog: newBlog
+    });
+});
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
